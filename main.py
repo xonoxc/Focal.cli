2bbf1c0f552cf747265301022dcb364241a813bc
arpit <arpit2003y@gmail.com>
2025-09-11 20:39:52 +0530

    chore(metadata):added_additional_usage_meta_data

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", contents="Why is the sky blue?"
     )
-    print(response.text)
+    print("response:", response.text)
+    print("tokens_used:", response.usage_metadata.prompt_token_count)
+    print("candidate_token_count:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
